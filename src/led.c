12e34157f08d406f9e2f64148826eefd15904824
barrybingo <millions.of.stones@gmail.com>
2013-03-09 14:56:05 +0000

    Removed PB8,9 so only on-board LEDs are included. Tidied up source a bit.

--- src/led.c
+++ src/led.c
@@ -18,71 +18,59 @@
 #include "stm32f10x_conf.h"
 #include "led.h"
 
-GPIO_TypeDef* GPIO_PORT[LEDn] = {LED1_GPIO_PORT, LED2_GPIO_PORT, LED3_GPIO_PORT, LED4_GPIO_PORT};
-const uint16_t GPIO_PIN[LEDn] = {LED1_PIN, LED2_PIN, LED3_PIN, LED4_PIN};
-const uint32_t GPIO_CLK[LEDn] = {LED1_GPIO_CLK, LED2_GPIO_CLK, LED3_GPIO_CLK, LED4_GPIO_CLK};
+GPIO_TypeDef* LED_GPIO_PORT[NUM_LEDS] = {LED1_GPIO_PORT, LED2_GPIO_PORT};
+const uint16_t LED_GPIO_PIN[NUM_LEDS] = {LED1_PIN, LED2_PIN};
+const uint32_t LED_GPIO_CLK[NUM_LEDS] = {LED1_GPIO_CLK, LED2_GPIO_CLK};
 
 /**
   * @brief  Configures LED GPIO.
-  * @param  Led: Specifies the Led to be configured.
-  *   This parameter can be one of following parameters:
-  *     @arg LED1
-  *     @arg LED2
+  * @param  led: Specifies the LED to be configured.
   * @retval None
   */
-void LEDInit(Led_TypeDef Led)
+void LEDInit(Led_TypeDef led)
 {
   GPIO_InitTypeDef  GPIO_InitStructure;
 
   /* Enable the GPIO_LED Clock */
-  RCC_APB2PeriphClockCmd(GPIO_CLK[Led], ENABLE);
+  RCC_APB2PeriphClockCmd(LED_GPIO_CLK[led], ENABLE);
 
   /* Configure the GPIO_LED pin */
-  GPIO_InitStructure.GPIO_Pin = GPIO_PIN[Led];
+  GPIO_InitStructure.GPIO_Pin = LED_GPIO_PIN[led];
   GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
   GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
 
-  GPIO_Init(GPIO_PORT[Led], &GPIO_InitStructure);
+  GPIO_Init(LED_GPIO_PORT[led], &GPIO_InitStructure);
 
   /* start with LED off */
-  GPIO_PORT[Led]->BRR = GPIO_PIN[Led];
+  LED_GPIO_PORT[led]->BRR = LED_GPIO_PIN[led];
 }
 
 /**
-  * @brief  Turns selected LED On.
-  * @param  Led: Specifies the Led to be set on.
-  *   This parameter can be one of following parameters:
-  *     @arg LED1
-  *     @arg LED2
+  * @brief  Turns selected LED on.
+  * @param  led: Specifies the LED to be set on.
   * @retval None
   */
-void LEDOn(Led_TypeDef Led)
+void LEDOn(Led_TypeDef led)
 {
-  GPIO_PORT[Led]->BSRR = GPIO_PIN[Led];
+	LED_GPIO_PORT[led]->BSRR = LED_GPIO_PIN[led];
 }
 
 /**
-  * @brief  Turns selected LED Off.
-  * @param  Led: Specifies the Led to be set off.
-  *   This parameter can be one of following parameters:
-  *     @arg LED1
-  *     @arg LED2
+  * @brief  Turns selected LED off.
+  * @param  led: Specifies the LED to be set off.
   * @retval None
   */
-void LEDOff(Led_TypeDef Led)
+void LEDOff(Led_TypeDef led)
 {
-  GPIO_PORT[Led]->BRR = GPIO_PIN[Led];
+	LED_GPIO_PORT[led]->BRR = LED_GPIO_PIN[led];
 }
 
 /**
   * @brief  Toggles the selected LED.
-  * @param  Led: Specifies the Led to be toggled.
-  *   This parameter can be one of following parameters:
-  *     @arg LED1
-  *     @arg LED2
+  * @param  led: Specifies the LED to be toggled.
   * @retval None
   */
-void LEDToggle(Led_TypeDef Led)
+void LEDToggle(Led_TypeDef led)
 {
-  GPIO_PORT[Led]->ODR ^= GPIO_PIN[Led];
+	LED_GPIO_PORT[led]->ODR ^= LED_GPIO_PIN[led];
 }
--- src/led.h
+++ src/led.h
@@ -5,8 +5,6 @@
   * @brief   STM32-MINI-V3.0 LED control
   *          LED_1 on-board LED1 @ A2
   *          LED_2 on-board LED1 @ A3
-  *          LED_3 connected to PB8
-  *          LED_4 connected to PB9
   ******************************************************************************
   */
 
@@ -14,14 +12,10 @@
 #ifndef __LED_H
 #define __LED_H
 
-#ifdef __cplusplus
- extern "C" {
-#endif
-
  /* Exported constants --------------------------------------------------------*/
 
 /* number of LED */
-#define LEDn                             4
+#define NUM_LEDS                             4
 
 /* LED hardware configuration */
 #define LED1_PIN                         GPIO_Pin_2
@@ -32,21 +26,11 @@
 #define LED2_GPIO_PORT                   GPIOA
 #define LED2_GPIO_CLK                    RCC_APB2Periph_GPIOA
 
-#define LED3_PIN                         GPIO_Pin_8
-#define LED3_GPIO_PORT                   GPIOB
-#define LED3_GPIO_CLK                    RCC_APB2Periph_GPIOB
-
-#define LED4_PIN                         GPIO_Pin_9
-#define LED4_GPIO_PORT                   GPIOB
-#define LED4_GPIO_CLK                    RCC_APB2Periph_GPIOB
-
 /* exported types -------------------------------------------------------------*/
 typedef enum
 {
   LED1 = 0,
   LED2 = 1,
-  LED3 = 0,
-  LED4 = 1,
 } Led_TypeDef;
 
 /* exported functions ---------------------------------------------------------*/
